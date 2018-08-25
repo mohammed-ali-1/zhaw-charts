69e113c7efc3fc044706c3ae9829e487e43b8d7a
mohammed-ali-1 <mohammad10596@gmail.com>
2018-08-25 17:56:31 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 status=`curl -X GET 'http://helmqa-helmqa.a3c1.starter-us-west-1.openshiftapps.com/livecheck?repo=https://github.com/appuio/charts.git' | jq .code`
 if [ $status -eq 404 ]; 
-then echo "error"; travis_terminate 1 
+then echo "ERROR!" 
 fi
